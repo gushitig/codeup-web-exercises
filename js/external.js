ac40e133e684e73777d51f14a794d64c4ebbdabb
Christina Antonuccio <antonuccio.christina@gmail.com>
2022-11-22 11:15:11 -0600

    send it

---
+++ js/external.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// console.log("Hello from external JavaScript!");
+//
+// alert("Welcome to my Website!")
+//
+// let color = prompt('What is your favorite color?');
+// alert(`Great, ${color} is my favorite color too!`);
+
+let pricePerDay= prompt('How much does it cost per day to rent a movie per day?');
+let littleMermaid = prompt('How many days are you renting The Little Mermaid?');
+let brotherBear = prompt('How many days are you renting Brother Bear?');
+let hercules = prompt('How many days are you renting Hercules?');
+//var totalPrice = ((littleMermaid + brotherBear + hercules) * pricePerDay);
+alert(`Your total cost will be $${(littleMermaid * pricePerDay) + (brotherBear * pricePerDay) + (hercules * pricePerDay)}.`);
